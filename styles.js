6b4ef991c093c444b3a1671b7b4c0210f675a8cf
ccna35 <shawkykhalil@gmail.com>
2021-07-25 18:42:07 +0200

    Moved script to js file

--- styles.js
+++ styles.js
@@ -1,11 +1,11 @@
 var MenuItems = document.getElementById("MenuItems");
 
-MenuItems.style.maxHeight = "0px";
+MenuItems.style.maxHeight = "0%";
 
 function showmenu() {
-  if (MenuItems.style.maxHeight == "0px") {
-    MenuItems.style.maxHeight = "200px";
+  if (MenuItems.style.maxHeight == "0%") {
+    MenuItems.style.maxHeight = "100%";
   } else {
-    MenuItems.style.maxHeight = "0px";
+    MenuItems.style.maxHeight = "0%";
   }
 }
